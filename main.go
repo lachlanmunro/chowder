72c8ee5c5cc7c178803e3f4af9055e5c8d3497d2
Lachlan Munro <lachlanmunro@users.noreply.github.com>
2022-04-16 21:50:32 +0100

    Invert flag defaults (pretty logs, int durations) to improve flag usage

--- main.go
+++ main.go
@@ -21,10 +21,10 @@ func main() {
 	antivirusURL := flag.String("antivirus", "127.0.0.1:3310", "Destination antivirus URL")
 	certFile := flag.String("certfile", "server.crt", "Server TLS certificate")
 	keyFile := flag.String("keyfile", "server.key", "Server TLS key")
-	pretty := flag.Bool("pretty", true, "Use pretty logging (instead of JSON)")
+	pretty := flag.Bool("pretty", false, "Use pretty logging (instead of JSON)")
 	usersFile := flag.String("usersfile", "users.yml", "Users file containing auth tokens in the format `token: username\\n`, if not supplied or empty authentication will be disabled")
 	unixTime := flag.Bool("unixtime", false, "Log unix timestamps instead of RFC3339Nano")
-	integerDurations := flag.Bool("intdur", true, "Log integer durations instead of floats")
+	floatDurations := flag.Bool("floatdur", false, "Log float durations instead of integers")
 	flag.Parse()
 	// Setup the logger
 	if *pretty {
@@ -43,14 +43,14 @@ func main() {
 		Bool("pretty", *pretty).
 		Str("usersfile", *usersFile).
 		Bool("unixtime", *unixTime).
-		Bool("intdur", *integerDurations).
+		Bool("floatdur", *floatDurations).
 		Logger()
 	loglevel, err := zerolog.ParseLevel(*level)
 	if err != nil {
 		l.Fatal().Err(err).Msg("loglevel must be one of debug, info, warn, error, fatal, panic")
 	}
 	zerolog.SetGlobalLevel(loglevel)
-	zerolog.DurationFieldInteger = *integerDurations
+	zerolog.DurationFieldInteger = !*floatDurations
 	// Get the auth list
 	f, err := ioutil.ReadFile(*usersFile)
 	if err != nil && !os.IsNotExist(err) {
